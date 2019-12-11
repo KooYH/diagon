5b3460d9577a07acd63f01b03a3ed96476597599
KooYH <dudgusl009@gmail.com>
2019-12-11 12:54:12 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,18 @@
-module.exports = {telegraf_token:'your telegraf token',
-database:'Your database connection string',
+const  getToken  = (function () {
+  const  token  =  process.env.TELEGRAM_TOKEN;
+  return  function () {
+    return  token;
+  };
+})();
+
+const  getDbcon  = (function () {
+  const  token  =  process.env.DB_CONN;
+  return  function () {
+    return  token;
+  };
+})();
+
+module.exports = {telegraf_token:getToken(),
+database:getDbcon(),
 secret:'yoursecret'};
 
